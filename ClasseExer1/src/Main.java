9d760635f7c6d0fab4030b69cb1d9b634dfda68e
gabrielraulino <gabrielviana@alu.ufc.br>
2022-10-07 11:00:16 -0300

    DesafioIntermediario

---
+++ ClasseExer1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  System.out.println("Iniciou o programa no método main.");
+  }
+  static void a(){
+    System.out.println("entrou no método a.");
+    b();
+    System.out.println("Finalizou o metodo a.");
+
+  }
+  static void b(){
+    si
+  }
+}
